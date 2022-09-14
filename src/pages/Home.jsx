2402ae0905a8130c8d3c081ee89bcf9d7e745bda
Богдан <norestwithwest@Overlord.local>
2022-09-14 16:29:00 +0300

    added toptextfield and slider components to a home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Slider from '../components/Slider'
+import TopTextField from '../components/TopTextField'
 
 const Home = () => {
   return (
     <div>
+        <TopTextField  />
         <Navbar/>
+        <Slider/>
     </div>
   )
 }
